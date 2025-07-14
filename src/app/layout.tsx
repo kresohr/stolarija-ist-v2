e438fd780f5971fc2f398621577e10b4fc878be0
kresohr <ilicic.kresimir@gmail.com>
2025-07-14 23:29:55 +0200

    Added all new components to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 "use client";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
-import { useState } from "react";
-import styles from "./Navbar.module.css";
+import { Navbar } from "@/components/navbar";
+import { Hero } from "@/components/hero";
+import { About } from "@/components/about";
+import { Projects } from "@/components/projects";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,39 +16,6 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-function Navbar() {
-  const [open, setOpen] = useState(false);
-  /* Implement state reset when window >= 600px */
-  return (
-    <nav className={styles.navbar} aria-label="Glavna navigacija">
-      <div className={styles["navbar-header"]}>
-        <span className={styles["navbar-title"]}>Stolarija IST</span>
-        <button
-          className={styles["navbar-toggle"]}
-          aria-label={open ? "Zatvori navigaciju" : "Otvori navigaciju"}
-          aria-expanded={open}
-          onClick={() => setOpen((prev) => !prev)}
-        >
-          {open ? "✖" : "☰"}
-        </button>
-      </div>
-      <ul className={`${styles["navbar-menu"]} ${open ? styles.open : ""}`}>
-        <li>
-          <Link href="/about">O nama</Link>
-        </li>
-        <li>
-          <Link href="/projekti">Projekti</Link>
-        </li>
-        <li>
-          <Link href="/kontakt" className={styles["navbar-contact"]}>
-            Kontakt
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  );
-}
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -55,9 +23,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Navbar />
-        {children}
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} h-screen overflow-hidden`}
+      >
+        <div className="h-full flex flex-col">
+          <Navbar />
+          <main className="flex-1 overflow-y-auto">
+            <Hero />
+            <About />
+            <Projects />
+            {children}
+          </main>
+        </div>
       </body>
     </html>
   );
